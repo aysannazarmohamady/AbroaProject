1ad3e1b9e2d46126a16bec828abe12df1084844e
Aysan Nazarmohammadi <30371881+aysannazarmohamady@users.noreply.github.com>
2024-06-11 19:49:40 +0330

    Update Save.py

--- ScraperEngine/Save.py
+++ ScraperEngine/Save.py
@@ -1,1 +1,23 @@
+import pandas as pd
 
+# Define the column names as a list
+columns = [
+    "Title of Position",
+    "University",
+    "Country",
+    "Supervisor",
+    "Email of Supervisor",
+    "LinkedIn of Supervisor",
+    "More Contacts",
+    "Deadline",
+    "About the position"
+]
+
+# Create an empty DataFrame with the specified columns
+Save = pd.DataFrame(columns=columns)
+
+# Print the empty DataFrame
+print(Save)
+
+# Save the DataFrame to a CSV file
+Save.to_csv('job_openings.csv', index=False)
