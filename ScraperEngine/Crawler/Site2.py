7edcc516b6cbbead42185b4b0ce36374ac15af65
Aysan Nazarmohammadi <30371881+aysannazarmohamady@users.noreply.github.com>
2024-06-15 16:44:50 +0330

    Update Site2.py

--- ScraperEngine/Crawler/Site2.py
+++ ScraperEngine/Crawler/Site2.py
@@ -1,1 +1,68 @@
+import requests
+from bs4 import BeautifulSoup
+
+def fetch_data_by_url(url, start_index):
+    # جایگزینی پارامتر صفحه در URL
+    paginated_url = url.format(i=start_index)
+    
+    # درخواست HTTP به وب‌سایت ارسال کنید
+    response = requests.get(paginated_url)
+    response.raise_for_status()  # چک کردن موفقیت درخواست
+    
+    # محتوای HTML را با BeautifulSoup پردازش کنید
+    soup = BeautifulSoup(response.text, 'html.parser')
+    
+    # پیدا کردن همه‌ی باکس‌هایی که ساختاری شبیه به توضیحات داده شده دارند
+    li_elements = soup.find_all('li')
+    
+    results = []
+    
+    # حلقه برای پیمایش و استخراج اطلاعات از هر باکس
+    for li in li_elements:
+        h4_tag = li.find('h4')
+        if h4_tag:
+            a_tag = h4_tag.find('a')
+            if a_tag:
+                title = a_tag.get_text(strip=True)
+                if 'PhD' in title:
+                    link = 'http://scholarshipdb.net' + a_tag['href']
+                    result = {'Title': title, 'Link': link}
+                    
+                    divs = li.find_all('div')
+                    if divs and len(divs) > 1:
+                        location_info = divs[1].get_text(strip=True)
+                        result['Location Info'] = location_info
+                    
+                    p_tag = li.find('p')
+                    if p_tag:
+                        description = p_tag.get_text(strip=True)
+                        result['Description'] = description
+                    
+                    results.append(result)
+    
+    return results
+
+# لیست URL های پایه
+urls = [
+    'http://scholarshipdb.net/PhD-scholarships-in-United-States?page={i}',
+    'http://scholarshipdb.net/PhD-scholarships-in-Canada?page={i}'
+]
+
+# حلقه برای پیمایش URLها و شماره صفحات
+all_data = []
+for base_url in urls:
+    for page_num in range(1, 5):  # حلقه برای تغییر صفحات یا پردازش مجموعه‌های داده مختلف
+        url = base_url.format(i=page_num)
+        data = fetch_data_by_url(url, page_num)
+        all_data.extend(data)
+
+# نمایش داده‌ها
+for item in all_data:
+    print(f"Title of Position: {item['Title']}")
+    print(f"Link: {item['Link']}")
+    if 'Location Info' in item:
+        print(f"Location Info: {item['Location Info']}")
+    if 'Description' in item:
+        print(f"Description: {item['Description']}")
+    print('-' * 50)
 
