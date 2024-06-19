164acdc4b72842007eff50e2183326dc660de986
Aysan Nazarmohammadi <30371881+aysannazarmohamady@users.noreply.github.com>
2024-06-19 17:39:57 +0330

    Update Site3.py

--- ScraperEngine/Crawler/Site3.py
+++ ScraperEngine/Crawler/Site3.py
@@ -1,1 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
 
+# لینک صفحه وب
+url = 'https://academicpositions.com/jobs/position/phd'
+
+# دریافت محتوای صفحه وب
+response = requests.get(url)
+soup = BeautifulSoup(response.content, 'html.parser')
+
+# پیدا کردن تمام بخش‌هایی که دارای کلاس card-body هستند
+positions = soup.find_all('div', class_='card-body')
+
+# استخراج لینک مربوط به هر پوزیشن
+for position in positions:
+    link = position.find('a', href=True)
+    if link:
+        print(link['href'])
