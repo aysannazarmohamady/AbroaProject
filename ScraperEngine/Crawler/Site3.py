c50f5fe62375d4f1d410d0b6346b35466a28929c
Aysan Nazarmohammadi <30371881+aysannazarmohamady@users.noreply.github.com>
2024-06-20 13:47:48 +0330

    Update Site3.py

--- ScraperEngine/Crawler/Site3.py
+++ ScraperEngine/Crawler/Site3.py
@@ -1,18 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
 
-# لینک صفحه وب
-url = 'https://academicpositions.com/jobs/position/phd'
+# آدرس وب‌سایتی که می‌خواهیم از آن داده‌ها را استخراج کنیم
+url = 'https://academicpositions.com/jobs/position/phd?sort=recent'
 
-# دریافت محتوای صفحه وب
+# درخواست برای دریافت صفحه
 response = requests.get(url)
-soup = BeautifulSoup(response.content, 'html.parser')
 
-# پیدا کردن تمام بخش‌هایی که دارای کلاس card-body هستند
-positions = soup.find_all('div', class_='card-body')
+# چک کردن اینکه آیا درخواست موفق بوده است
+if response.status_code == 200:
+    # استفاده از BeautifulSoup برای تجزیه HTML
+    soup = BeautifulSoup(response.text, 'html.parser')
 
-# استخراج لینک مربوط به هر پوزیشن
-for position in positions:
-    link = position.find('a', href=True)
-    if link:
-        print(link['href'])
+    # پیدا کردن دیو مورد نظر که شامل لیست شغل‌ها است
+    job_list = soup.find('div', id='list-jobs')
+
+    # بررسی هر کارت شغلی در داخل دیو مورد نظر
+    job_cards = job_list.find_all('div', class_='card shadow-sm mb-4 mb-md-4 job-list-item')
+    for card in job_cards:
+        job_link = card.find('a', class_='text-dark text-decoration-none hover-title-underline job-link')
+        if job_link:
+            job_title = job_link.find('h4')
+            if job_title:
+                print("Job title:", job_title.text.strip())
+                print("Job link:", job_link['href'])
+else:
+    print("Failed to retrieve the page. Status code:", response.status_code)
