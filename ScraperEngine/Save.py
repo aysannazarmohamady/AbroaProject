4faf7ac28d1824e0209a8de0e2fac51712987a55
Aysan Nazarmohammadi <30371881+aysannazarmohamady@users.noreply.github.com>
2024-06-23 20:19:01 +0330

    Update Save.py

--- ScraperEngine/Save.py
+++ ScraperEngine/Save.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
 # Load the CSV files
-df1 = pd.read_csv('site1.csv')
-df2 = pd.read_csv('site2.csv')
-df3 = pd.read_csv('site3.csv')
+df1 = pd.read_csv('website1.csv')
+df2 = pd.read_csv('website2.csv')
+df3 = pd.read_csv('website3.csv')
 
 # Concatenate the DataFrames
 df_combined = pd.concat([df1, df2, df3], ignore_index=True)
 
 # Save the combined DataFrame to a new CSV file
-df_combined.to_csv('combined_sites.csv', index=False)
+df_combined.to_csv('combinedd_sites.csv', index=False)
 
 print("Combined CSV file has been created successfully.")
